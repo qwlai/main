7ac5c6c7d82c3b2304ab97f3f2797be5c2aa11a9
qwlai <laiqiwei30@hotmail.com>
2018-03-11 18:22:54 +0800

    Add basic structure for theater.

---
+++ src/main/java/seedu/address/model/cinema/Theater.java
@@ -0,0 +1,21 @@
+package seedu.address.model.cinema;
+
+/**
+ * Represents a theater in a cinema
+ */
+public class Theater {
+
+    private int theaterNumber;
+
+    public Theater(int theaterNumber) {
+        this.theaterNumber = theaterNumber;
+    }
+
+    public int getTheaterNumber() {
+        return theaterNumber;
+    }
+
+    public void setTheaterNumber(int theaterNumber) {
+        this.theaterNumber = theaterNumber;
+    }
+}
